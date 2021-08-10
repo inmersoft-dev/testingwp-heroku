40c1349c8c11e49fa925d25b872ae5e4c9a482a3
SCiytcor <carlos.morag@estudiantes.uo.edu.cu>
2021-08-09 18:15:27 -0700

    json

---
+++ js/fetchJSON.js
@@ -0,0 +1,11 @@
+fetch('people.json')
+  .then((response) => {
+    return response.json()
+  })
+  .then((data) => {
+    // Work with JSON data here
+    console.log(data);
+  })
+  .catch((err) => {
+    // Do something for an error here
+  })
